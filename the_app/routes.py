dcf7862816704081e1f1d700dad7ec90f1d439c8
moises cantero lopez <moisescantero@gmail.com>
2020-09-06 13:48:57 +0200

    acabado apartado de capitanías y añadido en base.html

--- the_app/routes.py
+++ the_app/routes.py
@@ -74,6 +74,134 @@ def rents_040705_boato_jeque():
 def rents_040008_boato_rojo_dorado():
     return render_template("rents_040008_boato_rojo_dorado.html")
 
+@app.route("/rents_captains")
+def rents_captains():
+    return render_template("rents_captains.html")
+
+@app.route("/rents_012019_capitan_beig_rosa")
+def rents_012019_capitan_beig_rosa():
+    return render_template("rents_012019_capitan_beig_rosa.html")
+
+@app.route("/rents_022019_capitana_beig_rosa")
+def rents_022019_capitana_beig_rosa():
+    return render_template("rents_022019_capitana_beig_rosa.html")
+
+@app.route("/rents_012018_capitan_azul_plata")
+def rents_012018_capitan_azul_plata():
+    return render_template("rents_012018_capitan_azul_plata.html")
+
+@app.route("/rents_022018_capitana_azul_plata")
+def rents_022018_capitana_azul_plata():
+    return render_template("rents_022018_capitana_azul_plata.html")
+
+@app.route("/rents_012016_capitan_rojo_plata")
+def rents_012016_capitan_rojo_plata():
+    return render_template("rents_012016_capitan_rojo_plata.html")
+
+@app.route("/rents_022016_capitana_rojo_plata")
+def rents_022016_capitana_rojo_plata():
+    return render_template("rents_022016_capitana_rojo_plata.html")
+
+@app.route("/rents_012015_capitan_blanco_oro")
+def rents_012015_capitan_blanco_oro():
+    return render_template("rents_012015_capitan_blanco_oro.html")
+
+@app.route("/rents_022015_capitana_blanco_oro")
+def rents_022015_capitana_blanco_oro():
+    return render_template("rents_022015_capitana_blanco_oro.html")    
+
+@app.route("/rents_012014_capitan_negro_oro")
+def rents_012014_capitan_negro_oro():
+    return render_template("rents_012014_capitan_negro_oro.html")
+
+@app.route("/rents_022014_capitana_negro_oro")
+def rents_022014_capitana_negro_oro():
+    return render_template("rents_022014_capitana_negro_oro.html")
+
+@app.route("/rents_012013_capitan_ibi_marron")
+def rents_012013_capitan_ibi_marron():
+    return render_template("rents_012013_capitan_ibi_marron.html")
+
+@app.route("/rents_022013_capitana_ibi_marron")
+def rents_022013_capitana_ibi_marron():
+    return render_template("rents_022013_capitana_ibi_marron.html")
+
+@app.route("/rents_012015_capitan_azul_verde")
+def rents_012015_capitan_azul_verde():
+    return render_template("rents_012015_capitan_azul_verde.html")
+
+@app.route("/rents_022015_capitana_azul_verde")
+def rents_022015_capitana_azul_verde():
+    return render_template("rents_022015_capitana_azul_verde.html")
+
+@app.route("/rents_012012_capitan_rosa_plata")
+def rents_012012_capitan_rosa_plata():
+    return render_template("rents_012012_capitan_rosa_plata.html")
+
+@app.route("/rents_022012_capitana_rosa_plata")
+def rents_022012_capitana_rosa_plata():
+    return render_template("rents_022012_capitana_rosa_plata.html")
+
+@app.route("/rents_012013_capitan_ibi_ecuador")
+def rents_012013_capitan_ibi_ecuador():
+    return render_template("rents_012013_capitan_ibi_ecuador.html")
+
+@app.route("/rents_022013_capitana_ibi_ecuador")
+def rents_022013_capitana_ibi_ecuador():
+    return render_template("rents_022013_capitana_ibi_ecuador.html")
+
+@app.route("/rents_012010_capitan_ibi_morado")
+def rents_012010_capitan_ibi_morado():
+    return render_template("rents_012010_capitan_ibi_morado.html")
+
+@app.route("/rents_022010_capitana_ibi_morado")
+def rents_022010_capitana_ibi_morado():
+    return render_template("rents_022010_capitana_ibi_morado.html")
+
+@app.route("/rents_012010_capitan_blanco_plata")
+def rents_012010_capitan_blanco_plata():
+    return render_template("rents_012010_capitan_blanco_plata.html")
+
+@app.route("/rents_022010_capitana_blanco_plata")
+def rents_022010_capitana_blanco_plata():
+    return render_template("rents_022010_capitana_blanco_plata.html")
+
+@app.route("/rents_012009_capitan_verde_marron")
+def rents_012009_capitan_verde_marron():
+    return render_template("rents_012009_capitan_verde_marron.html")
+
+@app.route("/rents_022009_capitana_verde_marron")
+def rents_022009_capitana_verde_marron():
+    return render_template("rents_022009_capitana_verde_marron.html")
+
+@app.route("/rents_012008_capitan_rojo_negro")
+def rents_012008_capitan_rojo_negro():
+    return render_template("rents_012008_capitan_rojo_negro.html")
+
+@app.route("/rents_022008_capitana_rojo_negro")
+def rents_022008_capitana_rojo_negro():
+    return render_template("rents_022008_capitana_rojo_negro.html")
+
+@app.route("/rents_012008_capitan_marron")
+def rents_012008_capitan_marron():
+    return render_template("rents_012008_capitan_marron.html")
+
+@app.route("/rents_022008_capitana_marron")
+def rents_022008_capitana_marron():
+    return render_template("rents_022008_capitana_marron.html")
+
+@app.route("/rents_012007_capitan_azul_blanco")
+def rents_012007_capitan_azul_blanco():
+    return render_template("rents_012007_capitan_azul_blanco.html")
+
+@app.route("/rents_022007_capitana_azul_blanco")
+def rents_022007_capitana_azul_blanco():
+    return render_template("rents_022007_capitana_azul_blanco.html")
+
+@app.route("/rents_022006_capitana_rojo_beig")
+def rents_022006_capitana_rojo_beig():
+    return render_template("rents_022006_capitana_rojo_beig.html")
+
 
 @app.route("/sales")
 def sales():
